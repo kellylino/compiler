5178885a3843040726f423a879dcfc3603c6baa6
Haiyuan Lin <haiyuan.lin@tuni.fi>
2026-01-20 00:02:20 +0200

    token

---
+++ src/compiler/tokenizer.py
@@ -0,0 +1,88 @@
+import re
+from typing import Literal, Union
+from dataclasses import dataclass
+
+TokenType = Literal['int_literal', 'identifier', 'operators', 'punctuation', 'other']
+
+class SpecialLocation:
+    def __eq__(self, other: object) -> bool:
+        return True
+
+L = SpecialLocation()
+
+@dataclass
+class Location:
+    line: int
+    column: int
+
+@dataclass
+class Token:
+    text: str
+    type: Literal['int_literal', 'identifier', 'operators', 'punctuation', 'other']
+    loc: Union[Location, SpecialLocation]
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Token):
+            if self.text != other.text or self.type != other.type:
+                return False
+
+            return (self.loc == L or other.loc == L or self.loc == other.loc)
+        return False
+
+def tokenize(source_code: str) -> list[Token]:
+    tokens = []
+
+    identifiers = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
+    non_negative_integer = re.compile(r'[0-9][0-9]*')
+    operators = re.compile(r'==|!=|<=|>=|\+|\-|\*|/|%|=|<|>')
+    punctuation = re.compile(r'[\(\)\{\},;]')
+
+    patterns: list[tuple[re.Pattern[str], TokenType]] = [
+        (identifiers, 'identifier'),
+        (non_negative_integer, 'int_literal'),
+        (operators, 'operators'),
+        (punctuation, 'punctuation')
+    ]
+
+    i = 0
+    line = 1
+    column = 1
+    while i < len(source_code):
+        char = source_code[i]
+
+        if char == '\n':
+            line += 1
+            column = 1
+            i += 1
+            continue
+
+        if char.isspace():
+            column += 1
+            i += 1
+            continue
+
+        if source_code.startswith('//', i) or source_code.startswith('#', i):
+            while i < len(source_code) and source_code[i] != '\n':
+                i += 1
+            continue
+
+        loc = Location(line, column)
+
+        matched = False
+        for pattern, token_type in patterns:
+            match = pattern.match(source_code, i)
+            if match:
+                text = match.group(0)
+                tokens.append(Token(text, token_type, loc))
+
+                i += len(text)
+                column += len(text)
+                matched = True
+                break
+
+        if not matched:
+            tokens.append(Token(char, 'other', loc))
+            i += 1
+            column += 1
+
+    return tokens
---
+++ tests/tokenizer_test.py
@@ -0,0 +1,66 @@
+from compiler.tokenizer import tokenize, Token, L
+
+def test_tokenizer_basics() -> None:
+    # assert tokenize("if  3\nwhile") == ['if', '3', 'while']
+
+    assert tokenize('aaa 123 bbb') == [
+        Token(loc=L, type="identifier", text="aaa"),
+        Token(loc=L, type="int_literal", text="123"),
+        Token(loc=L, type="identifier", text="bbb"),
+    ]
+
+def test_tokenizer_operators_and_punctuation() -> None:
+    src = 'aaa == { } + - * / % = != <= >= < > ; ,'
+    expected = [
+        Token(text="aaa", type="identifier", loc=L),
+        Token(text="==", type="operators", loc=L),
+        Token(text="{", type="punctuation", loc=L),
+        Token(text="}", type="punctuation", loc=L),
+        Token(text="+", type="operators", loc=L),
+        Token(text="-", type="operators", loc=L),
+        Token(text="*", type="operators", loc=L),
+        Token(text="/", type="operators", loc=L),
+        Token(text="%", type="operators", loc=L),
+        Token(text="=", type="operators", loc=L),
+        Token(text="!=", type="operators", loc=L),
+        Token(text="<=", type="operators", loc=L),
+        Token(text=">=", type="operators", loc=L),
+        Token(text="<", type="operators", loc=L),
+        Token(text=">", type="operators", loc=L),
+        Token(text=";", type="punctuation", loc=L),
+        Token(text=",", type="punctuation", loc=L),
+    ]
+    assert tokenize(src) == expected
+
+def test_tokenizer_skips_comments() -> None:
+    src = 'aaa 123 // this is a comment\nbbb # another comment'
+    expected = [
+        Token(text="aaa", type="identifier", loc=L),
+        Token(text="123", type="int_literal", loc=L),
+        Token(text="bbb", type="identifier", loc=L),
+    ]
+    assert tokenize(src) == expected
+
+def test_tokenizer_if_statement() -> None:
+    source = """if a < 10 then {
+            print_int(3*x);  # this here is a comment
+            }"""
+
+    expected_tokens = [
+        Token(loc=L, type="identifier", text="if"),
+        Token(loc=L, type="identifier", text="a"),
+        Token(loc=L, type="operators", text="<"),
+        Token(loc=L, type="int_literal", text="10"),
+        Token(loc=L, type="identifier", text="then"),
+        Token(loc=L, type="punctuation", text="{"),
+        Token(loc=L, type="identifier", text="print_int"),
+        Token(loc=L, type="punctuation", text="("),
+        Token(loc=L, type="int_literal", text="3"),
+        Token(loc=L, type="operators", text="*"),
+        Token(loc=L, type="identifier", text="x"),
+        Token(loc=L, type="punctuation", text=")"),
+        Token(loc=L, type="punctuation", text=";"),
+        Token(loc=L, type="punctuation", text="}"),
+    ]
+
+    assert tokenize(source) == expected_tokens
